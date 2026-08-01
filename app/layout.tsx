5706e4d790d1836f2c0f55d09bd9d55c363f72e2
Ezzio <ezzio@nullhypothesis.com>
2026-08-01 17:05:01 +0300

    Step 5: Remove maximumScale and userScalable

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,8 +43,6 @@ export const metadata: Metadata = {
 export const viewport: Viewport = {
   width: "device-width",
   initialScale: 1,
-  maximumScale: 1,
-  userScalable: false,
 };
 
 import { TransitionLink } from "@/components/TransitionLink/TransitionLink";
